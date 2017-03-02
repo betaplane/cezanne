661ea26ed0eeb03cfc606eb6c0cc6cec7c9e39ce
Arno Hammann <ac@hammann.de>
2017-03-02 17:43:50 -0300

    corrected errors in radiation, added daily ave extraterr. availability.ipynb

--- python/radiation.py
+++ python/radiation.py
@@ -18,100 +18,142 @@ sta = D['sta']
 T = hh.extract(D['ta_c'], 'prom', C2K=True)
 
 
-So = 1361
-sun = ep.Sun()
-rad = np.pi / 180
-
 def stationize(df):
     try:
         return pd.DataFrame(df, columns=df.columns.get_level_values('station'))
     except:
-        return pd.DataFrame(df, index=df.index.get_level_values('station')
+        return pd.DataFrame(df, index=df.index.get_level_values('station'))
 
 
-# hour angle
-def hour(obs, t):
-    obs.date = t
-    sun.compute(obs)
-    return obs.sidereal_time() - sun.ra
+class observer(ep.Observer):
+    # https://en.wikipedia.org/wiki/Solar_constant
+    S0 = 1361
+    rad = np.pi / 180
+    def __init__(self, station):
+        super(ep.Observer, self).__init__()
+        self.sun = ep.Sun()
+        self.lon = station.lon * self.rad
+        self.lat = station.lat * self.rad
+        self.elevation = station.elev
 
-# declination
-def dec(obs, t):
-    obs.date = t
-    sun.compute(obs)
-    return sun.dec
+    def allparams(self, t):
+        """
+        Returns hour angle, sun's declination, sun-earth distance in AU.
+        See https://en.wikipedia.org/wiki/Hour_angle.
+        """
+        self.date = t
+        self.sun.compute(self)
+        return self.sidereal_time() - self.sun.ra, self.sun.dec, self.sun.earth_distance
+
+    def alt_dist(self, t):
+        """
+        Returns sun's zenith angle based on PyEphem Observer and time, and as convenience also the
+        earth-sun distance in AU
+        See http://rhodesmill.org/pyephem/quick.html#bodies.
+        """
+        self.date = t
+        self.sun.compute(self)
+        return self.sun.alt, self.sun.earth_distance
+
+    def decl(self, t):
+        self.date = t
+        self.sun.compute(self)
+        return self.sun.dec, self.sun.earth_distance
+
+    def angle_decl(self, t):
+        self.date = t
+        self.sun.compute(self)
+        return self.sidereal_time() - self.sun.ra, self.sun.dec
 
 
 # extra-terrestrial radiation
 # Allen, R.G., Pereira, L.S., Raes, D., Smith, M., 1998. FAO Irrigation and drainage paper No. 56.
 # Rome: Food and Agriculture Organization of the United Nations 56, 97–156.
-def extraterrestrial(stations, time):
-    t = pd.date_range(time[0], time[-1] + np.timedelta64(1, 'h'), freq='H')
-    Ra = pd.DataFrame(index=time)
-    for c, r in sta.iterrows():
-        print(c)
-        obs = ep.Observer()
-        obs.lon = r.lon * rad
-        obs.lat = r.lat * rad
-        obs.elevation = r.elev
-        f = partial(hour, obs)
-        g = partial(dec, obs)
+
+def ETRaDay(stations, index):
+    "Daily average extraterrestrial radiation from explicit integral over hour angle (as in Allen et al.)."
+    def per_station(code, station):
+        print(code)
+        obs = observer(station)
+        dec, dist = np.array([obs.decl(i) for i in index]).T
+        ws = np.arccos(-np.tan(obs.lat) * np.tan(dec))
+        return 1/np.pi * obs.S0 * dist**-2 * (ws * np.sin(obs.lat) * np.sin(dec) + np.cos(dec) * np.sin(ws))
+    return pd.DataFrame(dict([(c, per_station(c, r)) for c,r in stations.iterrows()]), index=index)
+
+
+def ETRa1(stations, index):
+    "Subdaily extraterrestrial radiation based on explicit integral over hour angle interval given by Allen et al. 1998."
+    t = pd.date_range(index[0], index[-1] + np.timedelta64(1, 'h'), freq='H')
+    def per_station(code, station):
+        print(code)
+        obs = observer(station)
         # this is to account for the averaging period in the database
-        te = t + np.timedelta64(4, 'h') - np.timedelta64(int(r.interval), 's')
-        w = np.array([f(i) for i in te])
-        d = np.array([g(i) for i in te])
-        ws = np.arccos(-np.tan(obs.lat) * np.tan(d))
+        te = t + np.timedelta64(4, 'h') - np.timedelta64(int(station.interval), 's')
+        w, dec, dist = np.array([obs.allparams(i) for i in te]).T
         w[w > np.pi] -= 2 * np.pi
         w[w < -np.pi] += 2 * np.pi
-        w[w < -ws] = 0
-        w[w > ws] = 0
-        w1 = w[:-1]
-        w2 = w[1:]
-        d = (d[:-1] + d[1:]) / 2
-        R = 12 / np.pi * So * (1 + 0.033 * np.cos(2 * np.pi / 365 * t[:-1].dayofyear)) * \
-        ((w2 - w1) * np.sin(obs.lat) * np.sin(d) + np.cos(obs.lat) * np.cos(d) * (np.sin(w2) - np.sin(w1)))
+        # sunset hour angle
+        ws = np.arccos(-np.tan(obs.lat) * np.tan(dec))
+        # mask, where shift should ensure that intervals containing sunrise or -set are retained
+        m = np.where(w > -ws, 1, 0)[1:] * np.where(w < ws, 1, 0)[:-1]
+        # setting angles to sunset in between should compute correct integrals for partial hours at sunset/-rise
+        # note thought that ws is recomputed throughout the night, so differences are not exactly zero - 
+        # hence the mask
+        w = np.where(w > -ws, w, -ws)
+        w = np.where(w < ws, w, ws)
+        # R = 12 / np.pi * S0 * (1 + 0.033 * np.cos(2 * np.pi / 365 * t[:-1].dayofyear)) * \
+        R = 12 / np.pi * obs.S0 * dist[1:]**-2 * \
+        (np.diff(w) * np.sin(obs.lat) * np.sin(dec[1:]) + np.cos(obs.lat) * np.cos(dec[1:]) * np.diff(np.sin(w)))
         R[R < 0] = 0
-        Ra = pd.concat((Ra, pd.Series(R, index=rs.index, name=c)), axis=1)
-    return Ra
+        return {'Ra':R*m, 'mask':m}
+    # return per_station(*next(stations.iterrows()))
+    p = pd.Panel(dict([(c, per_station(c,r)) for c,r in stations.iterrows()]))
+    p.major_axis = index
+    return p
 
 
-def solalt(obs, t):
-    """
-    Returns sun's zenith angle based on PyEphem Observer and time, and as convenience also the
-    earth-sun distance in AU
-    See http://rhodesmill.org/pyephem/quick.html#bodies.
-    """
-    obs.date = t
-    sun.compute(obs)
-    return (sun.alt, sun.earth_distance)
-
-
-def extraterrestrial2(stations, t):
-    S0 = 1370
-    sun = ep.Sun()
-    rad = np.pi / 180
-    def per_station(c,r):
-        print(c)
-        obs = ep.Observer()
-        obs.lon = r.lon * rad
-        obs.lat = r.lat * rad
-        obs.elevation = r.elev
-        z = partial(solalt, obs)
+def ETRa2(stations, t):
+    "Subdaily extraterrestrial radiation by using interval midpoint zenith computed directly bu PyEphem."
+    def per_station(code, station):
+        print(code)
+        obs = observer(station)
         # here, shift the time vector by each station's averaging period so it contains the interval endpoints
         # add 1/2 hour to use midpoints (i.e. 270 mins together with UTC shift)
-        te = t + np.timedelta64(270, 'm') - np.timedelta64(int(r.interval), 's')
-        alt,d = np.array([z(i) for i in te]).T
-        # compute averages over mu (= cos zenith, = sin alt) analytically
+        te = t + np.timedelta64(270, 'm') - np.timedelta64(int(station.interval), 's')
+        alt, d = np.array([obs.alt_dist(i) for i in te]).T
+        # mu = cos zenith = sin alt
         mu = np.sin(alt)
         mu[mu<0] = 0
-        return S0 * (1/d)**2 * mu
-    return pd.DataFrame(dict([(c, per_station(c,r)) for c,r in stations.iterrows()]), index=time)
+        return obs.S0 * d**-2 * mu
+    return pd.DataFrame(dict([(c, per_station(c,r)) for c,r in stations.iterrows()]), index=t)
 
 
-Ra = extraterrestrial2(sta, rs.index)
+def mask_night(stations, index):
+    t = pd.date_range(index[0], index[-1] + np.timedelta64(1, 'h'), freq='H')
+    def per_station(code, station):
+        print(code)
+        obs = observer(station)
+        # this is to account for the averaging period in the database
+        te = t + np.timedelta64(4, 'h') - np.timedelta64(int(station.interval), 's')
+        w, dec = np.array([obs.angle_decl(i) for i in te]).T
+        w[w > np.pi] -= 2 * np.pi
+        w[w < -np.pi] += 2 * np.pi
+        # sunset hour angle
+        ws = np.arccos(-np.tan(obs.lat) * np.tan(dec))
+        # before sunrise
+        # this shifts the sunrise index to one earlier, so that the interval containing the sunrise is retained
+        m[:-1] = np.logical_or(m[:-1], m[1:])
+        # after sunset
+        return m * np.where(w < ws, 1, 0)[:-1]
+    return pd.DataFrame(dict([(c, per_station(c,r)) for c,r in stations.iterrows()]), index=index)
+
+
+Rm = ETRaDay(sta, rs.index)
+Ra = ETRa1(sta, rs.index)
 # Ra = D['Ra_w']
 
-def Rso
+def Rso1(Ra, z):
+    return (.75 + 2e-5 * z) * Ra
 
 
 def regression(rm, Ra):
@@ -151,18 +193,18 @@ def regression(rm, Ra):
             b.append((a, (err/n)**.5, n))
     return pd.DataFrame(b, index=rm.columns, columns=['b', 'rms', 'n'])
 
+def quality():
+    b = regression(rs, Ra).sort_index()
+    # manual selection of sensors at stations which have 2 - for now take the ones with lower RMS
+    from CEAZAMet import mult
+    c = b.loc[mult(rs)]
+    drop_codes = ['28', 'ANDACMP10', 'CACHRSTM', 'COMBRS', 'INILLARS', 'MINRS', 'PCRS2', 'TLHRSTM']
+    b.drop(drop_codes,level='code',inplace=True)
+    rs.drop(drop_codes,level='code',inplace=True,axis=1)
 
-b = regression(rs, Ra).sort_index()
-# manual selection of sensors at stations which have 2 - for now take the ones with lower RMS
-from CEAZAMet import mult
-c = b.loc[mult(rs)]
-drop_codes = ['28', 'ANDACMP10', 'CACHRSTM', 'COMBRS', 'INILLARS', 'MINRS', 'PCRS2', 'TLHRSTM']
-b.drop(drop_codes,level='code',inplace=True)
-rs.drop(drop_codes,level='code',inplace=True,axis=1)
-
-# very high regression coefficients are also suspicious
-b.sort_values('b', inplace=True)
-drop_codes.extend(['INIA66', 'PAGN', 'LMBT', 'SALH', ''])
+    # very high regression coefficients are also suspicious
+    b.sort_values('b', inplace=True)
+    drop_codes.extend(['INIA66', 'PAGN', 'LMBT', 'SALH', ''])
 
 def detect_breaks(df):
     i = df.dropna().index
@@ -171,7 +213,6 @@ def detect_breaks(df):
 def residuals(code):
     return pd.concat((b['b'][code][0] * Ra[code], rs[code]), axis=1).diff(1, axis=1).iloc[:,1]
 
-rs['TASC'][:'2014-12-07 14']
 
 def itpl():
     "Interpolate 4D (true) temp and (mass level) geopotential fields to stations."
@@ -188,12 +229,12 @@ def itpl():
     Gi = interp4D((x, y), GP, ij, sta.index, t, method='linear')
 
 
-S = pd.HDFStore('../../data/tables/LinearLinear.h5')
-R = pd.HDFStore('../../data/tables/4Dfields.h5')
-Z = S['z']['d02']
-T2 = S['T2']['d02']
-T4D = R['temp']
-G4D = R['ghgt']
+# S = pd.HDFStore('../../data/tables/LinearLinear.h5')
+# R = pd.HDFStore('../../data/tables/4Dfields.h5')
+# Z = S['z']['d02']
+# T2 = S['T2']['d02']
+# T4D = R['temp']
+# G4D = R['ghgt']
 
 def temp_AGL(T,G,Z,z):
     "Compute mean T difference between 'z' meters above ground and T2 from model data (model ground level)."
@@ -248,7 +289,7 @@ def cloud_N(Rs, Rso):
 def LW_isothermal(Tr, N):
     return -5.67e-8 * Tr**4 * (1 - 9.35e-6 * Tr**2) + 60 * N
 
-stationize(b)
-Tr = T.apply(lambda c:c+a[c.name],0)
-N = cloud_N(rs, Ra*b['b'])
-Li = LW_isothermal(Tr, N)
+# stationize(b)
+# Tr = T.apply(lambda c:c+a[c.name],0)
+# N = cloud_N(rs, Ra*b['b'])
+# Li = LW_isothermal(Tr, N)
