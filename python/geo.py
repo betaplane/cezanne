b28828813d41780d0e2a30fab9214b4e9c3c8f11
Arno Hammann <ac@hammann.de>
2017-03-14 17:20:50 -0300

    some bugs squeezed in geo.py

--- python/geo.py
+++ python/geo.py
@@ -11,41 +11,49 @@ class angle(object):
         self.map = basemap.Basemap(
             projection = 'merc',
             llcrnrlon = self._geo[0],
-            llcrnrlat = self._geo[3] - ds.RasterYSize * self._geo[5],
-            urcrnrlon = self._geo[0] + ds.RasterXSize * self._geo[1],
+            llcrnrlat = self._geo[3] - gdalDS.RasterYSize * self._geo[5],
+            urcrnrlon = self._geo[0] + gdalDS.RasterXSize * self._geo[1],
             urcrnrlat = self._geo[3]
             )
         self._i = np.arange(gdalDS.RasterXSize, dtype=np.int)
         self._j = np.arange(gdalDS.RasterYSize, dtype=np.int)
-        self.z = ds.GetRasterBand(1).ReadAsArray()
+        self.z = gdalDS.GetRasterBand(1).ReadAsArray()
+        lon, lat = np.meshgrid(self._i + .5, self._j + .5)
+        # self._geo[5] is already negative
+        self._xy = self.map(self._geo[0] + lon * self._geo[1], self._geo[3] + lat * self._geo[5])
 
 
     def set_station(self, station):
-        self._x = int((station.lon - self._geo[0]) // self._geo[1])
-        self._y = int((station.lat - self._geo[3]) // self._geo[5])
-        # subtract station elevation from grid
-        # first dim of matrix is lat, second lon
-        z = self.z[self._y, self._x]
-        self.z -= z
-        print('elevation difference station - grid: {:.0f} m'.format(station.elev - z))
-        lon, lat = np.meshgrid(self._i + .5, self._j + .5)
-        # self._geo[5] is already negative
-        x, y = self.map(self._geo[0] + lon * self._geo[1], self._geo[3] + lat * self._geo[5])
+        self.station_coords(station, get=False)
         sx, sy = self.map(station.lon, station.lat)
-        self.dist = ((x - sx)**2 + (y - sy)**2)**.5
-        self.angle = np.arctan(self.z / self.dist)
+        try:
+            z = self.z[self.sj, self.si]
+        except:
+            raise Exception('{} outside map boundaries'.format(station.name))
+        dz = z - station.elev
+        print('elevation difference grid - station: {:.0f} m'.format(dz))
+        self.dist = ((self._xy[0] - sx)**2 + (self._xy[1] - sy)**2)**.5
+        self.angle1 = np.arctan((self.z - z) / self.dist)
+        self.angle2 = np.arctan((self.z - station.elev) / self.dist)
+        return dz
 
-    def window(self, dx, x, y=None):
+    def station_coords(self, station=None, get=True):
+        if station is not None:
+            self.si = int((station.lon - self._geo[0]) // self._geo[1])
+            self.sj = int((station.lat - self._geo[3]) // self._geo[5])
+            if (self.si < 0) or (self.sj < 0) or (self.si >= self._i[-1]) or (self.sj >= self._j[-1]):
+                raise Exception('{} outside map boundaries'.format(station.name))
+        if get:
+            return self.sj, self.si
+
+
+    def window(self, dx, x=None, y=None):
         try:
             if len(x.shape) > 1:
-                return x[self._y-dx: self._y+dx, self._x-dx: self._x+dx]
+                return x[self.sj-dx: self._y+dx+1, self.si-dx: self.si+dx+1]
             else:
-                return x[self._x-dx: self._x+dx]
-                return y[self._y-dx: self._y+dx]
-        except:
-            pass
-        try:
-            return x - self._x + dx, y - self._y + dx
+                return x[self.si-dx: self.si+dx+1]
+                return y[self.sj-dx: self._y+dx+1]
         except:
             pass
 
@@ -53,7 +61,7 @@ class angle(object):
     # https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
     def path(self, a):
         """
-        Compute a path in pixel coordinates going through the station location (self._x, self._y)
+        Compute a path in pixel coordinates going through the station location (self.si, self.sj)
         at angle 'a' from east (normal math convention).
         :param a: Angle from east in radians.
         :returns: i, j vectors containing the pixel coordinates.
@@ -61,10 +69,10 @@ class angle(object):
         sa = np.sin(a)
         ca = np.cos(a)
         if ca == 0:
-            i = np.ones(len(self._j), dtype=np.int) * self._x
+            i = np.ones(len(self._j), dtype=np.int) * self.si
             return i, self._j
         if sa == 0:
-            j = np.ones(len(self._i), dtype=np.int) * self._y
+            j = np.ones(len(self._i), dtype=np.int) * self.sj
             return self._i, j
         else:
             ta = abs(np.tan(a))
@@ -73,27 +81,36 @@ class angle(object):
             if ta < 1:
                 j = np.round(-np.sign(sa) * ta * self._i).astype(int)
                 # shift so that line goes through station pixel
-                j += self._y - j[self._x]
+                j += self.sj - j[self.si]
                 ix = (j >= 0) & (j <= self._j[-1])
                 return self._i[ix], j[ix]
             # y > x
             else:
                 i = np.round(-np.sign(ca) * abs(ca / sa) * self._j).astype(int)
-                i += self._x - i[self._y]
+                i += self.si - i[self.sj]
                 ix = (i >= 0) & (i <= self._i[-1])
                 return i[ix], self._j[ix]
 
     def max_ang(self, ang, dn):
-        i, j = self.path(ang)
-        # remember i, j reversed
-        d = self.dist[j, i]
-        n = np.argmin(d)
-        a = self.angle[j, i]
-        m1 = np.argmax(a[n+1+dn:])
-        m2 = np.argmax(a[:n-dn])
-        return (a[n+1+m1], a[m2], d[n+1+dn+m1], d[m2]) if ang < np.pi/8 else (a[m2], a[n+1+m1], d[m2], d[n+1+dn+m1])
+        try:
+            i, j = self.path(ang)
+            # remember i, j reversed
+            d = self.dist[j, i]
+            n = np.argmin(d)
+            a = self.angle1[j, i]
+            def ang_dist(ang):
+                a = ang[j,i]
+                m1 = np.argmax(a[n+1+dn:])
+                m2 = np.argmax(a[:n-dn])
+                if ang < np.pi/8:
+                    return (a[n+1+dn+m1], a[m2], d[n+1+dn+m1], d[m2])
+                else:
+                    return (a[m2], a[n+1+dn+m1], d[m2], d[n+1+dn+m1])
+            return np.array([ang_dist(a) for a in [self.angle1, self.angle2]])
+        except:
+            return np.ones(8) * np.nan
 
-    def circle(self, res, from_north=True, station=None, dx=2):
+    def circle(self, res, station=None, from_north=True, dx=2):
         """
         Compute the elevation angle of mountains surrounding a station in a circle starting from north,
         counter-clockwise. Simple planar geometry is used, no atmospheric refraction.
@@ -105,18 +122,21 @@ class angle(object):
         and 'dist' (distance from station at which maximum elevation angle is found).
         """
         if station is not None:
-            print('Setting station {}'.format(station.name))
             try:
-                self.set_station(station)
-            except:
-                print('Station {} outside map.'.format(station.name))
-                return pd.DataFrame()
+                dz = self.set_station(station)
+            except Exception as excp:
+                print(excp)
+                return pd.DataFrame(), np.nan
             else:
                 print('Setting station {}.'.format(station.name))
 
         da = 2*np.pi / res * np.arange(res/2)
         c = np.array([self.max_ang(a, dx) for a in da]).T.flatten()
-        return pd.DataFrame(c.reshape((2, res)).T, columns=['alt', 'dist'], index=np.r_[da,da+np.pi])
+        return pd.DataFrame(
+            c.reshape((4, res)).T,
+            columns=['alt_grid', 'dist_grid', 'alt_st', 'dist_st'],
+            index=np.r_[da,da+np.pi]
+        ), dz
 
 
 if __name__ == "__main__":
@@ -126,8 +146,9 @@ if __name__ == "__main__":
     st = sta.loc['5']
 
     A = angle(ds)
-
-    P = pd.Panel(dict([(c, A.circle(360, station=st)) for c, st in sta.iterrows()]))
+    circles = [(c, A.circle(360, st)) for c, st in sta.iterrows()]
+    P = pd.Panel(dict([a[:2] for a in circles]))
+    dz = pd.Series([a[2] for a in circles], index=[a[0] for a in circles])
     R = pd.HDFStore('../../data/tables/radiation.h5')
     R['shading'] = P
     R.close()
