cb4ef5a931187a8649af1a2b7df27f4797e8fb05
Arno Hammann <ac@hammann.de>
2017-05-15 17:58:55 -0400

    vertical KDEs for soundings in WRF.ipynb

--- python/IGRAraw.py
+++ python/IGRAraw.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 import pandas as pd
 import numpy as np
-import zipfile, io, sys, re
+import sys, re
 from subprocess import Popen, PIPE
- 
+from pycurl import Curl
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+
 # raw observations
 # curl -O https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/CIM00085586-data.txt.zip
 # curl -O https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/ARM00087418-data.txt.zip
@@ -40,11 +43,20 @@ cols = (
 )
 
 
+def uz(x):
+    c,a,b = zip(*x)
+    d = list(zip(np.array(a)-1,b))
+    return d,c
+
+def eat(head, data, var):
+    i = [np.repeat(pd.datetime(*t[1:]),t[0]) for t in
+         zip(head['NUMLEV'], head['YEAR'], head['MONTH'], head['DAY'], head['HOUR'])]
+    t = pd.DatetimeIndex([j for k in i for j in k])
+    data.index = pd.MultiIndex.from_arrays([t,data['PRESS']], names=('datetime', 'p'))
+    data = data.drop(-9999, 0, 'p').drop('PRESS', 1)
+    return data if var is None else data[var].unstack()
+
 def extract(file, var=None):
-    def uz(x):
-        c,a,b = zip(*x)
-        d = list(zip(np.array(a)-1,b))
-        return d,c
     z = zipfile.ZipFile(file)
     i = z.infolist()[0]
     b = z.open(i).read()
@@ -52,22 +64,37 @@ def extract(file, var=None):
     d,c = uz(head)
     p = Popen(['grep', '#'], stdin=PIPE, stdout=PIPE)
     out,err = p.communicate(input=b)
-    with io.BytesIO(out) as g:
+    with BytesIO(out) as g:
         H = pd.read_fwf(g,d,names=c)
 
     d,c = uz(cols)
     p = Popen(['sed', '-e', 's/^#.*$//'], stdin=PIPE, stdout=PIPE)
     out,err = p.communicate(input=b)
-    with io.BytesIO(out) as g:
+    with BytesIO(out) as g:
         D = pd.read_fwf(g,d,names=c).dropna(0,'all')
 
-    i = [np.repeat(pd.datetime(*t[1:]),t[0]) for t in
-         zip(H['NUMLEV'], H['YEAR'], H['MONTH'], H['DAY'], H['HOUR'])]
-    t = pd.DatetimeIndex([j for k in i for j in k])
-    D.index = pd.MultiIndex.from_arrays([t,D['PRESS']], names=('datetime', 'p'))
-    D = D.drop(-9999, 0, 'p').drop('PRESS', 1)
-    return D if var is None else D[var].unstack()
+    return eat(H, D, var)
 
+def get(name):
+    base = 'https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/{}-data.txt.zip'
+    buf = BytesIO()
+    c = Curl()
+    c.setopt(c.URL, base.format(name))
+    c.setopt(c.WRITEDATA, buf)
+    c.perform()
+    c.close()
+    z = ZipFile(buf)
+    out = z.open(z.infolist()[0]).read()
+    z.close()
+    return out.decode()
+
+def parse(string, var=None):
+    l = string.splitlines()
+    d, c = uz(head)
+    H = pd.read_fwf(StringIO('\n'.join([r for r in l if r[0]=='#'])), d, names=c)
+    d, c = uz(cols)
+    D = pd.read_fwf(StringIO('\n'.join([r for r in l if r[0]!='#'])), d, names=c)
+    return eat(H, D, var)
 
 
 if __name__ == "__main__":
