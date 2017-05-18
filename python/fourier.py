413d1368ec2295b0ecf83eabce76a092077919ed
Arno Hammann <ac@hammann.de>
2017-05-18 18:06:09 -0400

    LombScargle coherence in fourier.py

--- python/fourier.py
+++ python/fourier.py
@@ -4,47 +4,65 @@ import pandas as pd
 import scipy.signal as sig
 
 
-def welch(t, x, n):
-    w = sig.hann(n, sym=False)
-    d = n // 2
-    m = np.mean([LombScargle(t[j*d:j*d+n], x[j*d:j*d+n] * w).power(1, normalization='psd')
-        for j in range(len(x) // d - 1)])
-    return 2 * np.sqrt(m / n)
+def hann(df, start, width):
+    w = (0.5 - 0.5 * np.cos(2 * np.pi * (df.index.to_series() - start) / width))
+    print(len(w))
+    return df * w, len(w)**.5 / np.sum(w)
 
 class LS(object):
     def __init__(self, df, tshift=0):
         t = np.array(df.index, dtype='datetime64[m]').astype(int)
-        base_f = np.timedelta64(1,'Y').astype('timedelta64[m]').astype(int)
-        self._t = np.r_['1,2', t].T / base_f
-        self._y = df
+        period = np.timedelta64(1,'Y').astype('timedelta64[m]').astype(int)
+        self._y = df.copy()
+        self._y.index = t / period
         self._tf = tshift
 
     def tau(self, w):
         w2 = 2 * w
-        wt = self._t * w
+        t = np.r_['1,2', self._y.index].T
+        wt = t * w2
         tau = 1 / w2 * np.arctan(np.sum(np.sin(wt), 0) / np.sum(np.cos(wt), 0))
-        tp = self._t - tau
-        self._F0 = 1/(2**.5) * np.exp(-1j * w * (self._tf - tau))
-        self._wtp = tp * w
+        tp = t - tau
+        self._F0 = 2**-.5 * np.exp(-1j * w * (self._tf - tau))
+        self._wtp = pd.DataFrame(tp * w, index=self._y.index)
 
-    def Xk(self, f):
-        self.tau(2 * np.pi * f)
-        def dft(c):
-            j = c.notnull()
-            y = c[j]
-            wtp = self._wtp[j,:]
-            A = np.sum(np.cos(wtp)**2, 0) ** (-0.5)
-            B = np.sum(np.sin(wtp)**2, 0) ** (-0.5)
-            return pd.Series(self._F0 *
-                             (A * y.dot(np.cos(wtp)) + 1j * B * y.dot(np.sin(wtp))),
-                              index = f
-                          )
-        return self._y.apply(dft, 0)
+    def _lsdft(self, c, norm=1):
+        y = c.dropna()
+        wtp = self._wtp.loc[y.index].as_matrix()
+        A = np.sum(np.cos(wtp)**2, 0) ** (-0.5)
+        B = np.sum(np.sin(wtp)**2, 0) ** (-0.5)
+        return norm * pd.Series(self._F0 * (A * y.dot(np.cos(wtp)) + 1j * B * y.dot(np.sin(wtp))))
 
-    def coherence(self, f):
-        X = self.Xk(f)
-        return np.abs(X.iloc[:,0] * np.conj(X.iloc[:,1])) ** 2 / \
-            ((np.abs(X.iloc[:,0]) ** 2) * (np.abs(X.iloc[:,1]) ** 2))
+    def raw(self, f=None):
+        if f is not None:
+            self.tau(2 * np.pi * f)
+        x = self._y.apply(self._lsdft, 0)
+        x.index = f # needs changing
+        return x
+
+    def welch(self, n, f=None):
+        if f is not None:
+            self.tau(2 * np.pi * f)
+        def lsdft(c):
+            print(c.name)
+            y = c.dropna()
+            start = y.index[0]
+            T = y.index[-1] - start
+            w = T / n
+            def win(i):
+                s = start+i*w/2
+                return self._lsdft(*hann(y[(y.index>=s) & (y.index<=s+w)], s, w))
+            return pd.concat([win(i) for i in range(2 * n - 1)], 1)
+        x = pd.Panel(dict([(name, lsdft(col)) for name, col in self._y.iteritems()]))
+        x.major_axis = f
+        return x
+
+    def coherence(self, n=8, f=None):
+        G = self.welch(n, f)
+        XY = (G.iloc[0] * np.conj(G.iloc[1])).mean(1)
+        return np.abs(XY)**2 / (np.mean(G.iloc[0].abs()**2, 1) * np.mean(G.iloc[1].abs()**2, 1))
+
+
 
 if __name__=="__main__":
     from astropy.stats import LombScargle
