1bb9bc6c59033ec5a4a7d86281570258246c52f7
Arno Hammann <ac@hammann.de>
2017-05-31 17:51:15 -0400

    working time window in laplace.py

--- python/laplace.py
+++ python/laplace.py
@@ -16,12 +16,21 @@ import matplotlib.pyplot as plt
 # In SDM, 324–332. SIAM, 2013. http://epubs.siam.org/doi/abs/10.1137/1.9781611972832.36.
 
 class GLR(object):
-    def __init__(self, reg_mask, coeff_mask=None):
+    def __init__(self, reg_mask, coeff_mask=None, time_mask=None):
         self.W = xr.DataArray(reg_mask, dims=('space', 'i'))
         # self.W = 1 - self._w / self._w.max()
-        self.L = xr.DataArray(np.diag(np.array(coeff_mask.sum(1))) - coeff_mask, dims=('space', 'i'))
+        L = np.kron(time_mask, coeff_mask)
+        i = pd.MultiIndex.from_product(([0,1], [0,1]))
+        self.L = xr.DataArray(np.diag(np.array(L.sum(1))) - L, coords=[i, i])
 
-    def regress(self, X, Y, lda, plot=None):
+    def _block(self, X, Y):
+        x, y = xr.broadcast(X * self.W, Y * self.W, exclude=['var'])
+        C = (x * y).sum(('time', 'space')).transpose('i', 'var')
+        a = x.transpose('i','var','space','time')
+        A = la.block_diag(*np.einsum('ijkl,imkl->ijm', a, a))
+        return A, C
+
+    def regress(self, X, Y, lda, window=None, step=None):
         """
         :param X: predictor
         :type X: xarray with dims 'var', 'space', 'time'
@@ -30,38 +39,52 @@ class GLR(object):
         :param lda: regularization parameter (lambda)
         :param plot: None or location from 'space' dim to plot
         """
-        x, y = xr.broadcast(X * self.W, Y * self.W, exclude=['var'])
-        C = (x * y).sum(('time', 'space')).transpose('i', 'var')
-        a = x.transpose('i','var','space','time')
-        A = la.block_diag(*np.einsum('ijkl,imkl->ijm', a, a))
-        i = a['i']
-        L = np.kron(self.L.loc[i,i], np.identity(a['var'].size))
-        p = la.solve(A + lda * L, np.array(C).flatten()).reshape(a.shape[:2])
-        self.p = xr.DataArray(p, coords=[a['i'], a['var']], dims=['i', 'var'])
-        if plot is not None:
-            self.plot(X, Y, plot)
+        i = pd.MultiIndex.from_product(([0, 1], X['space']))
+        s = X['var'].size
+        w = window // 2
+        L = self.L.loc[i, i]
+        A, C = zip(*[self._block(
+            X.isel(time=slice(k, k+window)),
+            Y.isel(time=slice(k, k+window))
+        ) for k in range(0, Y['time'].size, step)])
+        L = np.kron(L, np.identity(s))
+        A = la.block_diag(*A)
+        C = np.vstack(C).flatten()
+        p = la.solve(A + lda * L, C).reshape((len(i), s))
+        P = xr.DataArray(p, coords=[i, X['var']], dims=['i', 'var']).unstack('i')
+        self.p = P.rename({'i_level_0': 'time', 'i_level_1': 'space'})
+
+    @staticmethod
+    def slicer(X, t, p):
+        pass
+
 
     def plot(self, X, Y, loc):
         fig = plt.figure()
         x, y, w = xr.broadcast(X.sel(var=0), Y, self.W.sel(i=loc))
         plt.scatter(x, y, c=w)
         plt.colorbar()
-        plt.scatter(x.sel(space=loc), y.sel(space=loc), color='r')
+        plt.scatter(x.sel(space=loc[0]), y.sel(space=loc[0]), color='r')
         xl = plt.gca().get_xlim()
         z = np.linspace(xl[0], xl[1], 100)
-        plt.plot(z, self.p.sel(i=loc, var=1).values + z * self.p.sel(i=loc, var=0).values, '-')
+        a = self.p.sel(time=loc[0], space=loc[1], var=1).values
+        b = self.p.sel(time=loc[0], space=loc[1], var=0).values
+        plt.plot(z, a + b * z, '-')
         fig.show()
 
     @classmethod
-    def test(cls, icpt=[0, 1], slope=[3, 3], reg=0, coeff=0, lda=0, plot=0):
-        x = np.linspace(0, 1, len(icpt)*100).reshape((100, -1))
-        y = [slope] * x + np.random.rand(len(icpt)*100).reshape(x.shape) / 2 + [icpt]
+    def test(cls, icpt=[[0, 1], [2, 3]], slope=[[3, -1], [1, -5]], reg=0, coeff=0, time=0, lda=0, plot=0):
+        x = np.atleast_3d(np.linspace(0, 1, 100)).transpose((1,0,2))
+        y = [slope] * x + [icpt]
+        y = y + np.random.randn(*y.shape) / 10
+        x = (x * np.ones(np.shape(icpt))).transpose((1,0,2)).reshape((-1,2))
         x = np.r_['0,3', x, np.ones(x.shape)]
         X = xr.DataArray(x, dims=('var', 'time', 'space'))
-        Y = xr.DataArray(y, dims=('time', 'space'))
-        I = np.identity(len(icpt))
-        r = cls(I+(1-I)*(1-reg), I+(1-I)*(1-coeff))
-        r.regress(X, Y, lda, plot)
+        Y = xr.DataArray(y.transpose((1,0,2)).reshape((-1,2)), dims=('time', 'space'))
+        I = np.identity(2)
+        r = cls(I+(1-I)*(1-reg), I+(1-I)*(1-coeff), I+(1-I)*(1-time))
+        r.regress(X, Y, lda, window=100, step=100)
+        r.plot(X, Y, plot)
         return r
 
 
