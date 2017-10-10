b40b8b65d7414914117859bb301d83f96196479f
Arno Hammann <ac@hammann.de>
2017-10-10 12:52:20 -0300

    datamod subpackage added

---
+++ python/datamod/CEAZA.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python
+import requests, csv
+from io import StringIO
+from dateutil.parser import parse
+from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from collections import Counter
+
+# url = 'http://192.168.5.2/ws/pop_ws.php'		# from inside
+
+
+
+class FetchError(Exception):
+    pass
+
+class NoNewStationError(Exception):
+    pass
+
+class _Reader(StringIO):
+    def __init__(self, str):
+        super(_Reader, self).__init__(str)
+        p = 0
+        while True:
+            try:
+                l = next(self)
+            except StopIteration:
+                raise FetchError(str)
+            if l[0] != '#':
+                break
+            self.start = p + l.find(':') + 1
+            p = self.tell()
+        self.seek(self.start)
+
+
+class Downloader(object):
+    """Class to download data from CEAZAMet webservice. Main reason for having a class is
+    to be able to reference the data (Downloader.data) in case something goes wrong at some point.
+    """
+    trials = range(10)
+    url = 'http://www.ceazamet.cl/ws/pop_ws.php'
+    raw_url = 'http://www.ceazamet.cl/ws/sensor_raw_csv.php'
+    max_workers = 16
+    from_date = datetime(2003, 1, 1)
+
+    field = {
+        'fn': 'GetListaSensores',
+        'p_cod': 'ceazamet',
+        'c0': 'tm_cod',
+        'c1': 's_cod',
+        'c2': 'tf_nombre',
+        'c3': 'um_notacion',
+        'c4': 's_altura',
+        'c5': 's_primera_lectura',
+        'c6': 's_ultima_lectura'
+    }
+
+    station = {
+        'fn': 'GetListaEstaciones',
+        'p_cod': 'ceazamet',
+        'c0': 'e_cod',
+        'c1': 'e_nombre',
+        'c2': 'e_lon',
+        'c3': 'e_lat',
+        'c4': 'e_altitud',
+        'c5': 'e_primera_lectura',
+        'c6': 'e_ultima_lectura'
+    }
+
+    def _get(self, f, from_date, raw):
+        if from_date is None:
+            if 'last' in f[1]:
+                day = f[1]['last'].to_pydatetime()
+                day = day - timedelta(days = 1) if day == day else self.from_date
+            else:
+                day = self.from_date
+        else:
+            day = from_date
+        try:
+            df = self.fetch_raw(f[0][2], day) if raw else self.fetch(f[0][2], day)
+        except FetchError as fe:
+            print(fe)
+        else:
+            i = df.columns.size
+            df.columns = pd.MultiIndex.from_arrays(
+                np.r_[
+                    np.repeat([f[0][0], f[0][1], f[0][2], f[1]['elev']], i),
+                    df.columns
+                ].reshape((-1, i)),
+                names = ['station', 'field', 'sensor_code', 'elev', 'aggr']
+            )
+            print('fetched {} from {}'.format(f[0][2], f[0][0]))
+            return f[0][2], df.sort_index(1)
+
+
+    def get_field(self, var, var_table, from_date=None, raw=False):
+        """Collect data from CEAZAMet webservice, for one variable type but all stations.
+
+        :param var: variable code to be collected (e.g. 'ta_c')
+        :param var_table: pandas.DataFrame with field metadata as constructed by get_stations()
+        :param from_date: initial date from which onward to request data
+        :param raw: False (default) or True whether raw data should be collected
+        :returns: data for one variable and all stations given by var_table
+        :rtype: pandas.DataFrame or dict with DataFrames if raw==True
+
+        """
+        with ThreadPoolExecutor(max_workers=self.max_workers) as exe:
+            self.data = [exe.submit(self._get, c, from_date=from_date, raw=raw) for c in
+                         var_table.xs(var, 0, 'field', False).iterrows()]
+
+        data = dict([d.result() for d in as_completed(self.data) if d.result() is not None])
+        return data if raw else pd.concat(data.values(), 1).sort_index(axis=1)
+
+    def get_fields_by_station(self, station, var_table, from_date=None, raw=False):
+        with ThreadPoolExecutor(max_workers=self.max_workers) as exe:
+            self.data = [exe.submit(self._get, c, from_date=from_date, raw=raw) for c in
+                         var_table.xs(station, 0, 'station', False).iterrows()]
+        data = [d.result()[1] for d in as_completed(self.data) if d.result() is not None]
+        return pd.concat(data, 1).sort_index(1)
+
+    def fetch(self, code, from_date=None):
+        from_date = self.from_date if from_date is None else from_date
+        cols = ['ultima_lectura', 'min', 'prom', 'max', 'data_pc']
+        params = {
+            'fn': 'GetSerieSensor',
+            'interv': 'hora',
+            'valor_nan': 'nan',
+            's_cod': code,
+            'fecha_inicio': from_date.strftime('%Y-%m-%d'),
+            'fecha_fin': (datetime.utcnow() - timedelta(hours=4)).strftime('%Y-%m-%d')
+            }
+        for trial in self.trials:
+            r = requests.get(self.url, params=params)
+            if not r.ok:
+                continue
+            reader = _Reader(r.text)
+            try:
+                d = pd.read_csv(
+                    reader, index_col=0, parse_dates=True, usecols=cols)
+                reader.close()
+            except:
+                raise FetchError(r.url)
+            else:
+                return d.astype(float) # important
+
+    def fetch_raw(self, code, from_date=None):
+        from_date = self.from_date if from_date is None else from_date
+        params = {'fi': from_date.strftime('%Y-%m-%d'),
+                  'ff': (datetime.utcnow() - timedelta(hours=4)).strftime('%Y-%m-%d'), 
+                  's_cod': code}
+        for trial in self.trials:
+            r = requests.get(self.raw_url, params=params)
+            if not r.ok:
+                continue
+            reader = _Reader(r.text)
+            try:
+                d = pd.read_csv(
+                    reader,
+                    index_col = 1,
+                    parse_dates = True
+                )
+                reader.close()
+            except:
+                raise FetchError(r.url)
+            else:
+                # hack for lines from webservice ending in comma - pandas adds additional column
+                # and messes up names
+                cols = [x.lstrip() for x in d.columns][-3:]
+                d = d.iloc[:,1:4]
+                d.columns = cols
+                return d.astype(float) # important
+
+    def get_stations(self, sta=None):
+        for trial in self.trials:
+            req = requests.get(self.url, params=self.station)
+            if not req.ok:
+                continue
+            with StringIO(req.text) as sio:
+                try:
+                    self.stations = [(l[0], l[1:6]) for l in csv.reader(sio) if l[0][0] != '#']
+                except:
+                    print('attempt #{}'.format(trial))
+                else:
+                    break
+
+        if sta is not None:
+            self.stations = [(c, st) for c, st in self.stations if c not in sta.index]
+
+        if len(self.stations) == 0:
+            raise NoNewStationError
+
+        stations = pd.DataFrame.from_items(
+            self.stations,
+            columns = ['full', 'lon', 'lat', 'elev', 'first'],
+            orient='index'
+        )
+        stations.index.name = 'station'
+
+        def get(st):
+            params = self.field.copy()
+            params['e_cod'] = st[0]
+            for trial in self.trials:
+                print(st[1].full)
+                req = requests.get(self.url, params=params)
+                if not req.ok:
+                    continue
+                with StringIO(req.text) as sio:
+                    try:
+                        return [((st[0], l[0], l[1]), l[2:6])
+                                  for l in csv.reader(sio) if l[0][0] != '#']
+                    except:
+                        print('attempt #{}'.format(trial))
+
+        with ThreadPoolExecutor(max_workers=self.max_workers) as exe:
+            fields = [exe.submit(get, s) for s in stations.iterrows()]
+
+        self.fields = [f for g in as_completed(fields) for f in g.result()]
+
+        fields = pd.DataFrame.from_items(
+            self.fields,
+            columns = ['full', 'unit', 'elev', 'first'],
+            orient = 'index'
+        )
+        fields.index = pd.MultiIndex.from_tuples(
+            fields.index.tolist(),
+            names = ['station', 'field', 'sensor_code']
+        )
+        return stations.sort_index(), fields.sort_index()
---
+++ python/datamod/IGRA.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import xarray as xr
+import sys, re
+from subprocess import Popen, PIPE
+from pycurl import Curl
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+import tarfile
+
+# raw observations
+# curl -O https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/CIM00085586-data.txt.zip
+# curl -O https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/ARM00087418-data.txt.zip
+
+head = (
+    ('HEADREC', 1, 1),
+    ('ID', 2, 12),
+    ('YEAR', 14, 17),
+    ('MONTH', 19, 20),
+    ('DAY', 22, 23),
+    ('HOUR', 25, 26),
+    ('RELTIME', 28, 31),
+    ('NUMLEV', 33, 36),
+    ('P_SRC', 38, 45),
+    ('NP_SRC', 47, 54),
+    ('LAT', 56, 62),
+    ('LON', 64, 71)
+)
+
+cols = (
+    ('LVLTYP1', 1, 1),
+    ('LVLTYP2', 2, 2),
+    ('ETIME', 4, 8),
+    ('PRESS', 10, 15),
+    ('PFLAG', 16, 16),
+    ('GPH', 17, 21),
+    ('ZFLAG', 22, 22),
+    ('TEMP', 23, 27),
+    ('TFLAG', 28, 28),
+    ('RH', 29, 33),
+    ('DPDP', 35, 39),
+    ('WDIR', 41, 45),
+    ('WSPD', 47, 51)
+)
+
+
+def uz(x):
+    c,a,b = zip(*x)
+    d = list(zip(np.array(a)-1,b))
+    return d,c
+
+def eat(head, data, var):
+    i = [np.repeat(pd.datetime(*t[1:]),t[0]) for t in
+         zip(head['NUMLEV'], head['YEAR'], head['MONTH'], head['DAY'], head['HOUR'])]
+    t = pd.DatetimeIndex([j for k in i for j in k])
+    data.index = pd.MultiIndex.from_arrays([t,data['PRESS']], names=('datetime', 'p'))
+    data = data.drop(-9999, 0, 'p').drop('PRESS', 1)
+    return data if var is None else data[var].unstack()
+
+def extract(file, var=None):
+    z = zipfile.ZipFile(file)
+    i = z.infolist()[0]
+    b = z.open(i).read()
+    z.close()
+    d,c = uz(head)
+    p = Popen(['grep', '#'], stdin=PIPE, stdout=PIPE)
+    out,err = p.communicate(input=b)
+    with BytesIO(out) as g:
+        H = pd.read_fwf(g,d,names=c)
+
+    d, c = uz(cols)
+    p = Popen(['sed', '-e', 's/^#.*$//'], stdin=PIPE, stdout=PIPE)
+    out, err = p.communicate(input=b)
+    with BytesIO(out) as g:
+        D = pd.read_fwf(g, d, names=c).dropna(0,'all')
+
+    return eat(H, D, var)
+
+def get(name):
+    base = 'https://www1.ncdc.noaa.gov/pub/data/igra/data/data-por/{}-data.txt.zip'
+    buf = BytesIO()
+    c = Curl()
+    c.setopt(c.URL, base.format(name))
+    c.setopt(c.WRITEDATA, buf)
+    c.perform()
+    c.close()
+    z = ZipFile(buf)
+    out = z.open(z.infolist()[0]).read()
+    z.close()
+    return out.decode()
+
+def parse(string, var=None):
+    l = string.splitlines()
+    d, c = uz(head)
+    H = pd.read_fwf(StringIO('\n'.join([r for r in l if r[0]=='#'])), d, names=c)
+    d, c = uz(cols)
+    D = pd.read_fwf(StringIO('\n'.join([r for r in l if r[0]!='#'])), d, names=c)
+    return eat(H, D, var)
+
+class Monthly(object):
+    """
+Some methods to parse monthly `IGRA (Integrated Global Radiosonde Archive) <https://www1.ncdc.noaa.gov/pub/data/igra/>`_ files (ending in '-mly.txt') collected into a class.
+    """
+    num = re.compile('(\d+)')
+    name = re.compile('([^_]+)_(\d+)')
+    station_cols = [(0, 11), (12, 20), (21, 30), (31, 37), (38, 40), (41, 71), (72, 76), (77, 81), (82, 88)]
+
+    @staticmethod
+    def _pivot(groupby):
+        df = groupby.copy()
+        df.index = pd.DatetimeIndex(['{}-{}'.format(*d) for d in df[[1, 2]].as_matrix()], name='time')
+        df = df.drop([0, 1, 2], 1)
+        df.columns = ['lvl', 'value', 'count']
+        return df.pivot(columns='lvl')
+
+    @classmethod
+    def txt_to_xarray(cls, filename, stations, numeric=True, zip=None):
+        """Return xarray.DataArray from IGRA monthly data files (zipped or unzipped). See also :meth:`tar_to_xarray`.
+
+        :param filename: name of regular text file or zipped file (need to give *zip* argument too)
+        :param stations: list of station ids (full string) to extract
+        :param numeric: if True, set the station dimension of the returned xarray.DataArray to integer values (if False, retain the alphanumeric station code
+        :param zip: if *filename* refers to a ZipFile, give here the name of the file to extract
+        :rtype: :class:`xarray.DataArray`
+
+        """
+        if zip is not None:
+            filename = BytesIO(ZipFile(filename).open(zip).read())
+        df = pd.read_csv(filename, header=None, delim_whitespace=True)
+        df = df[df[0].isin(stations)]
+        da = xr.DataArray(df.groupby(0).apply(cls._pivot)).unstack('dim_0').unstack('dim_1')
+        da = da.rename({'dim_0_level_0': 'station', 'dim_1_level_0': 'type'})
+        if numeric:
+            da['station'] = cls.station_to_int(da.station)
+        return da
+
+    @classmethod
+    def _untar(cls, tarf, info, *args, **kwargs):
+        var, hour = cls.name.search(info.name).groups()
+        da = cls.txt_to_xarray(BytesIO(tarf.extractfile(info).read()), *args, **kwargs)
+        da['time'] = da.time + pd.Timedelta(int(hour), 'h')
+        da.name = var
+        return da
+
+    @classmethod
+    def tar_to_xarray(cls, *args, tar='all', **kwargs):
+        """Wrapper around :meth:`txt_to_xarray` for tarfiles, with same arguments plus *tar*.
+
+        :param tar: If 'all', extract all files in archive, otherwise this should be the name of the individual file to extract from the tar archive.
+        :rtype: :class:`xarray.DataArray`
+
+        """
+        with tarfile.open(args[0]) as tarf:
+            if tar == 'all':
+                return xr.merge([cls._untar(tarf, m, *args[1:], **kwargs) for m in tarf.getmembers()])
+            else:
+                return cls.txt_to_xarray(BytesIO(tarf.extractfile(tar).read()), *args[1:], **kwargs)
+
+    @classmethod
+    def read_stations(cls, filename, **kwargs):
+        """Parse `station-list <https://www1.ncdc.noaa.gov/pub/data/igra/https://www1.ncdc.noaa.gov/pub/data/igra/igra2-station-list.txt>`_ file from `IGRA <https://www1.ncdc.noaa.gov/pub/data/igra/https://www1.ncdc.noaa.gov/pub/data/igra/>`_.
+
+        :param filename: file to parse.
+        :rtype: :class:`pandas.DataFrame`
+
+        """
+        sta = pd.read_fwf(filename, header=None, colspecs=cls.station_cols)
+        sta.columns = ['id', 'lat', 'lon', 'elev', 'state', 'name', 'first_year', 'last_year', 'nobs']
+        sta.index = cls.station_to_int(sta.id)
+        return sta
+
+    @classmethod
+    def station_to_int(cls, c):
+        return [int(cls.num.search(a).group(1)) for a in c.values]
+
+if __name__ == "__main__":
+    df = extract(sys.argv[1])
+    S = pd.HDFStore('IGRAraw.h5')
+    S[sys.argv[2]] = df
+    S.close()
