20da75c8bd33c6bef879dea98a7367f1c3451462
Arno Hammann <ac@hammann.de>
2017-10-11 15:15:33 -0300

    changed docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pycurl', 'pandas', 'xarray', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'cartopy']
+MOCK_MODULES = ['pycurl', 'pandas', 'xarray', 'numpy', 'matplotlib', 'matplotlib.pyplot', 'Cartopy', 'Shapely']
 sys.modules.update((name, Mock()) for name in MOCK_MODULES)
 
 
